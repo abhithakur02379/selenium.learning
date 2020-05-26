0c6c7d92177076d2ae8e51006157135da437e46c
Abhishek <abhithakur02379@gmail.com>
2020-05-26 07:30:17 +0530

    Code update on 26May_02

--- src/test/java/selenium/learning/Login.java
+++ src/test/java/selenium/learning/Login.java
@@ -2,11 +2,10 @@ package selenium.learning;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import test.utilities.InputRead;
 
 
 public class Login {
@@ -28,15 +27,15 @@ public class Login {
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\login\\Tools\\geckodriver\\geckodriver.exe");
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
-        driver.get("http://parabank.parasoft.com");
+        driver.get(InputRead.getProperty("baseURI"));
         Thread.sleep(5000);
         if (driver.findElement(By.xpath("//a[contains(text(),'ParaBank')]")).isDisplayed())
         {
             System.out.println("Home Page Loaded");
             if (driver.findElement(By.xpath("//*[contains(text(),'Customer Login')]")).isDisplayed())
             {
-                driver.findElement(By.xpath("//*[contains(@name,'username')]")).sendKeys("abhishek02379");
-                driver.findElement(By.xpath("//*[contains(@name,'password')]")).sendKeys("Password@2020");
+                driver.findElement(By.xpath("//*[contains(@name,'username')]")).sendKeys(InputRead.getProperty("username"));
+                driver.findElement(By.xpath("//*[contains(@name,'password')]")).sendKeys(InputRead.getProperty("password"));
                 driver.findElement(By.xpath("//*[contains(@value,'Log In')]")).click();
                 Thread.sleep(2000);
                 if (driver.findElement(By.xpath("//*[contains(text(),'Log Out')]")).isDisplayed())
